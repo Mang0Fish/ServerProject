89622f688d1aaae23cf5a94d7e4bb1eb12df20d2
Mang0Fish <oz241002@gmail.com>
2025-12-02 23:48:49 +0200

    catboost working plus utill funcs

--- main.py
+++ main.py
@@ -12,9 +12,8 @@ from routers.token import generate_tokens_for_user
 from fastapi import File, UploadFile
 import pandas as pd
 from io import StringIO
-from ml.training import train_example_model, train_logistic_reg
-from ml.utils import save_model
-from ml.utils import load_model
+from ml.training import train_logistic_reg, train_catboost_classifier, train_catboost_regressor
+from ml.utils import save_model, load_model, verify_file, train_model
 from fastapi import Body, Form
 
 """
@@ -46,62 +45,30 @@ DATABASE_URL = os.getenv("DATABASE_URL")
 app = FastAPI(title="EZPredict", description="Learn and predict using various models", version='1.0')
 app.include_router(token_router, prefix="/auth", tags=["auth"])
 
-
+"""
 @app.get("/funcCheckingg")
 def root(username: str, password: str):
     user = bl.verify_user(username, password)
     if not user:
         raise HTTPException(status_code=401, detail=f"Wrong Password or Username")
     return user
-
-
-
-# @app.post("/upload-csv/")
-# async def upload(file: UploadFile = File(...)):
-#     data = await file.read()
-#     csv = data.decode("utf-8")
-#     df = pd.read_csv(StringIO(csv))
-#     columns = df.columns.tolist()
-#     return {"filename": file.filename, "columns": columns}
+"""
 
 
 # Add user log in requirement !!!
 @app.post("/ml/train-csv")
 async def train_csv(file: UploadFile = File(...), label_column: str = Form(...)):
     # Step 1: read csv file into a DataFrame
+    if not file.filename.endswith(".csv"):
+        raise HTTPException(400, "File must be a CSV")
+
     data = await file.read()
-    csv_string = data.decode("utf-8")
-    df = pd.read_csv(StringIO(csv_string))
+    df = verify_file(data, label_column)
 
-    if label_column not in df.columns:
-        raise HTTPException(
-            status_code=400,
-            detail=f"Label column '{label_column}' does not exist"
-        )
+    model_trained = train_model(df, label_column)
+    return model_trained
 
-    features = [c for c in df.columns if c != label_column]
-    x = df[features]
-    y = df[label_column]
 
-    model, acc = train_logistic_reg(x, y)
-    saved_path = save_model(model)
-
-    return {
-        "msg": "CSV loaded",
-        "rows": len(df),
-        "features": features,
-        "label": label_column,
-        "accuracy": acc,
-        "model": saved_path
-    }
-
-# @app.post("/ml/train")
-# def train_model():
-#     model, acc = train_example_model()
-#
-#     saved_path = save_model(model)
-#
-#     return {"accuracy": acc, "model":saved_path}
 
 
 @app.post("/ml/predict")
--- ml/utils.py
+++ ml/utils.py
@@ -1,7 +1,10 @@
 import joblib
 from datetime import datetime
-from pathlib import Path
-import os
+from fastapi import HTTPException
+import pandas as pd
+from io import StringIO
+
+from ml.training import train_catboost_classifier, train_catboost_regressor
 
 
 def save_model(model):
@@ -17,3 +20,67 @@ def save_model(model):
 
 def load_model(path):
     return joblib.load(path)
+
+
+def verify_file(data, label_column):
+    if not data:
+        raise HTTPException(400, "The dataset is empty")
+
+    try:
+        csv_string = data.decode("utf-8")
+    except:
+        raise HTTPException(400, "Failed to decode the file a UTF-8 CSV")
+
+    df = pd.read_csv(StringIO(csv_string))
+
+    if df.empty:
+        raise HTTPException(400, "The dataset is empty")
+
+    if label_column not in df.columns:
+        raise HTTPException(
+            status_code=400,
+            detail=f"Label column '{label_column}' does not exist"
+        )
+
+    if len(df.columns) < 2:
+        raise HTTPException(400, "The dataset must contain at least 2 columns")
+
+    if df[label_column].nunique() < 2:
+        raise HTTPException(400, "The label column must contain at least 2 unique values")
+
+    if len(df) < 20:
+        raise HTTPException(400, "The dataset is too small to train the model")
+
+    return df
+
+
+def train_model(df, label_column):
+    y = df[label_column]
+    x = df.drop(columns=label_column)
+
+    cat_cols = [i for i, col in enumerate(x.columns) if x[col].dtype == "object"]
+
+    if y.dtype == "object":
+        problem_type = "classification"
+    elif y.nunique() <= 10:
+        problem_type = "classification"
+    else:
+        problem_type = "regression"
+
+    if problem_type == "classification":
+        model, score = train_catboost_classifier(x, y, cat_cols)
+    else:
+        model, score = train_catboost_regressor(x, y, cat_cols)
+
+    saved_path = save_model(model)
+
+    return {
+        "msg": "Training completed",
+        "rows": len(df),
+        "problem_type": problem_type,
+        "categorical_columns": cat_cols,
+        "features": x.columns.tolist(),
+        "label": label_column,
+        "score": score,
+        "model": saved_path
+    }
